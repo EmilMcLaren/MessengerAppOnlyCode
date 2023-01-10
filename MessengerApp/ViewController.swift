aca754d73d8f09c2170ff685e5434ec02c17f6e7
Emil <emper.1@yandex.ru>
2023-01-11 02:57:12 +0500

    Started commit

---
+++ MessengerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerApp
+//
+//  Created by Emil on 11.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
